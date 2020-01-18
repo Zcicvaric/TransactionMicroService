af58f70e6dfef4ffd6c98d5b374f80b52d79c4b7
unknown <zvonimircicvaric@windowslive.com>
2020-01-18 22:01:34 +0100

    added validateiban and validate argumentsForPaymentType

--- DB_model/src/main/java/oss/transaction/Bll/Interfaces/ITransactionValidatorService.java
+++ DB_model/src/main/java/oss/transaction/Bll/Interfaces/ITransactionValidatorService.java
@@ -6,4 +6,8 @@ import oss.transaction.Bll.Dtos.TransactionToCreateDto;
 public interface ITransactionValidatorService {
     public void validateTransaction(TransactionToCreateDto transaction) throws Exception;
     public void validateTransactionBeforeCancelation(TransactionToCancelDto transactionToCancel, long userId) throws Exception;
+    public void validateIban(String iban) throws Exception;
+    public void validateReferenceNumber(String referenceNumber) throws Exception;
+    public void validateUsageCode(String usageCode) throws Exception;
+    public void validateSWIFT(String swift) throws Exception;
 }
